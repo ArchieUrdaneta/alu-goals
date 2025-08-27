b66b5d7456865cbdb8368c90f3619d9aee5f39b2
heartgamboa <princejdbacalso@gmail.com>
2025-08-27 09:48:35 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBITK6bH07_JazvXdWiAoN8f_zn08qr3kk",
+  authDomain: "alu-goals.firebaseapp.com",
+  projectId: "alu-goals",
+  storageBucket: "alu-goals.firebasestorage.app",
+  messagingSenderId: "673913216257",
+  appId: "1:673913216257:web:f8ba161e2897a8eb148f00"
+};
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
